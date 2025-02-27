445d16d7732a1ff786af69d19a5dbe9588b92030
Oltion Abazi <oltion.abazi@gmail.com>
2025-02-27 11:01:38 +0100

    init

---
+++ app/events/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+import { View } from "react-native";
+
+const Events: FunctionComponent = () => {
+  return <View></View>;
+};
+
+export default Events;
